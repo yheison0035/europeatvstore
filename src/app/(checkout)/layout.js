4704fd40e9f248d911897a7ce8398c63d9ab46b7
Yeison Suarez <yheison0035@gmail.com>
2026-01-29 18:33:49 -0500

    se elimina script de wompi

--- src/app/(checkout)/layout.js
+++ src/app/(checkout)/layout.js
@@ -1,14 +1,9 @@
-import Script from "next/script";
 import SimpleHeader from "@/components/layout/checkout/components/simpleHeader";
 import CheckoutProviderWrapper from "@/context/checkoutProviderWrapper";
 
 export default function CheckoutLayout({ children }) {
   return (
     <CheckoutProviderWrapper>
-      <Script
-        src="https://checkout.wompi.co/widget.js"
-        strategy="beforeInteractive"
-      />
       <SimpleHeader />
       <main className="min-h-screen bg-(--bg-soft)">{children}</main>
     </CheckoutProviderWrapper>
